9e3ad86ee804946b8426149208645ec31c39f6cb
hyojeongindeyo <ghdgywjd510@naver.com>
2025-10-20 13:07:46 +0900

    five

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public calss Main {
     public static void main(String[] args) {
         System.out.println("홍효정");
         World.run();
+        World.run2();
     }
 }
--- World.java
+++ World.java
@@ -2,4 +2,8 @@ public class World {
     public static void run() {
         System.out.printlnn("202314156");
     }
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
 }
